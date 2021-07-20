05752807520cac5282bb2885ba0ab3565952a0d1
otvirtue@gmail.com <otvirtue@gmail.com>
2021-07-20 08:58:53 -0300

    new tables

---
+++ database/migrations/2021_07_19_113528_create_contacts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContactsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+            $table->string("Name",128);
+            $table->int("telephone",16);
+            $table->string("city",16);
+            $table->float("zip code",8);
+            $table->string("address",64);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contacts');
+    }
+}
